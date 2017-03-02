fe4004d9ce27fd8a8f7fd884744353b301f44665
George Tzinos <geotzinos@gmail.com>
2017-03-02 16:40:12 +0200

    FIX: One last old api url

--- NomikiAuth/src/pages/home/home.ts
+++ NomikiAuth/src/pages/home/home.ts
@@ -153,7 +153,7 @@ export class HomePage {
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
       
-    var url = "http://www.auth.l2koo.com/NomikiAuth/scraper/scraper-php-api/src/get-announcements.php";
+    var url = "http://www.law.l2koo.com";
     this.http.post(url, {"all": true},options).map(res => res.json())
     .subscribe(
           data => {
